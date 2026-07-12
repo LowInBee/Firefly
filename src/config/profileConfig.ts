199b8a36cb0c16a3f4a42309418405ee4ba9251b
LowInBee <1098866953@qq.com>
2026-07-12 12:41:52 +0800

    common update

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -30,13 +30,13 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/LowInBee",
 			showName: false,
 		},
 		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
+			url: "mailto:easontanx@qq.com",
 			showName: false,
 		},
 		{
